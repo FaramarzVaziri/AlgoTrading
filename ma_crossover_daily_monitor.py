f7da3fbd12e26cd8c8e1956240067ee9d88c5243
Faramarz Jabbarvaziri <65200008+FaramarzVaziri@users.noreply.github.com>
2024-07-08 10:23:04 -0700

    Update ma_crossover_daily_monitor.py
    buy/sell signals fixed.

--- ma_crossover_daily_monitor.py
+++ ma_crossover_daily_monitor.py
@@ -32,12 +32,12 @@ def analyze_strategies(initial_cash, start_date_test, end_date_test, long_window
 
     for i in range(len(data_test)):
         if data_test['Position_MA'].iloc[i] == 1.0:  # Buy signal
-            action = 'buy all $'+str(round(cash_ma)) + ' or keep shares if already bought'
+            action = 'buy all or keep shares if already bought'
             if cash_ma > 0:  # Ensure we only buy if we have cash
                 position_ma = cash_ma / data_test['Close'].iloc[i]
                 cash_ma = 0
         elif data_test['Position_MA'].iloc[i] == -1.0:  # Sell signal
-            action = 'sell all '+str(position_ma)+' shares or hold on if already sold'
+            action = 'sell all shares or hold on if already sold'
             if position_ma > 0:  # Ensure we only sell if we have a position
                 cash_ma = position_ma * data_test['Close'].iloc[i]
                 position_ma = 0
